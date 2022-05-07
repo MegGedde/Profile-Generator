767a7589772012e23ab4be8fde567a5b4acb2b1f
Megan Gedde <hoglu070@d.umn.edu>
2022-05-06 22:08:09 -0500

    added employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,34 @@
 const Employee = require('../lib/Employee');
+
+test('does Employee have properties', () => {
+    const employee = new Employee('Rachel');
+
+    expect(Employee.name).toBe('Rachel');
+    expect(Employee.id).toEqual(expect.any(Number));
+    expect(Employee.email).toEqual(expect.any(String));
+   });
+
+test("does name method work", () => {
+    const employee = new Employee('Rachel');
+    //check name
+    expect(employee.getName()).toEqual('Rachel');
+   });
+
+test("does Id method work", () => {
+    const employee = new Employee('Rachel');
+        //check ID
+        expect(employee.getId()).toEqual(expect.stringContaining(employee.Id.toString()));
+    });
+
+test("does email method work", () => {
+        const employee = new Employee('Rachel');
+        //check email
+        expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+    });
+
+test("does role method work", () => {
+        const employee = new Employee('Rachel');
+        //check role
+        expect(employee.getRole()).toEqual(expect.stringContaining(employee.email.toString()));
+    });
+
